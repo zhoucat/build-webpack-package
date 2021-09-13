67dd404d590dbd28a10e098d585edc92ef464f19
zhouGuang <zhou_learn@163.com>
2021-09-13 17:12:08 +0800

    git提交增加review机制

--- .cz-config.js
+++ .cz-config.js
@@ -2,31 +2,24 @@ module.exports = {
   // type 类型（定义之后，可通过上下键选择）
   types: [
     { value: 'feat', name: 'feat:     新增功能' },
-    { value: 'fix', name: 'fix:      修复 bug' },
+    { value: 'fix', name: 'fix:      功能变更 或者 修复bug' },
+    { value: 'refactor', name: 'refactor: 代码重构（不包括 bug 修复、功能新增）' },
+    { value: 'perf', name: 'perf:     优化/性能提升' },
+    { value: 'build', name: 'build:    构建流程、外部依赖变更（如升级 npm 包、修改 webpack 配置等）' },
+    { value: 'revert', name: 'revert:   回滚 commit/撤销修改' },
     { value: 'debug', name: 'debug:      排查问题' },
     { value: 'prefix', name: 'prefix:      预修复问题，不知道是否能解决' },
-    { value: 'docs', name: 'docs:     文档变更' },
+    { value: 'docs', name: 'docs:     文档/注释变更' },
     { value: 'style', name: 'style:    代码格式（不影响功能，例如空格、分号等格式修正）' },
-    { value: 'refactor', name: 'refactor: 代码重构（不包括 bug 修复、功能新增）' },
-    { value: 'perf', name: 'perf:     性能优化' },
     { value: 'test', name: 'test:     添加、修改测试用例' },
-    { value: 'build', name: 'build:    构建流程、外部依赖变更（如升级 npm 包、修改 webpack 配置等）' },
-    { value: 'ci', name: 'ci:       修改 CI 配置、脚本' },
-    { value: 'chore', name: 'chore:    对构建过程或辅助工具和库的更改（不影响源文件、测试用例）' },
-    { value: 'revert', name: 'revert:   回滚 commit' }
+    { value: 'mod', name: 'mod:     不确定分类的修改' },
+    
   ],
 
   // scope 类型（定义之后，可通过上下键选择）
   scopes: [
-    ['components', '组件 相关'],
-    ['page', '页面 相关'],
-    ['build', '构建 相关'],
-    ['styles', '样式 相关'],
-    ['deps', '项目依赖'],
-    ['docs', '文档 相关'],
-    ['other', '其他修改'],
-    // 如果选择 custom，后面会让你再输入一个自定义的 scope。也可以不设置此项，把后面的 allowCustomScopes 设置为 true
-    ['custom', '以上都不是？我要自定义']
+    ['custom', '推荐人'],
+    ['在家办公', ''],
   ].map(([value, description]) => {
     return {
       value,
@@ -58,9 +51,9 @@ module.exports = {
   // 交互提示信息
   messages: {
     type: '确保本次提交遵循 Angular 规范！\n选择你要提交的类型：',
-    scope: '\n选择一个 scope（可选）：',
+    scope: '\ncustom：',
     // 选择 scope: custom 时会出下面的提示
-    customScope: '请输入自定义的 scope：',
+    customScope: '请输入自定义的 review人或者不review的理由：',
     subject: '填写简短精炼的变更描述：\n',
     body:
       '填写更加详细的变更描述（可选）。使用 "|" 换行：\n',
