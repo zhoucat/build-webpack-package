141a0c67f87f840e34af0e3b8c939ac750c17cf4
zhouGuang <zhou_learn@163.com>
2021-09-14 15:54:38 +0800

    修复build环境变量配置报错问题

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,9 +1,18 @@
 'use strict'
+function transVal (val) {
+  if (!val && val !== 0) {
+    return null
+  }
+  if (val.indexOf('"') === -1) {
+    return '"' + val + '"'
+  }
+  return val
+}
 module.exports = {
   NODE_ENV: '"production"',
-  API_BASE_URL: process.env.API_BASE_URL || null,
-  ROUTER_BASE_URL: process.env.ROUTER_BASE_URL || null,
-  BUILD_PATH: process.env.BUILD_PATH || null,
-  RELATIVE: process.env.RELATIVE || null,
+  API_BASE_URL: transVal(process.env.API_BASE_URL),
+  ROUTER_BASE_URL: transVal(process.env.ROUTER_BASE_URL),
+  BUILD_PATH: transVal(process.env.BUILD_PATH),
+  RELATIVE: process.env.RELATIVE,
   PORT: process.env.PORT || 8080
 }
